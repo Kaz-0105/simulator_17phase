ee4d3f7ba995c841984aceda0b51c5adc132b9a1
Kazuki Takada <kazu0105barcafan@icloud.com>
2024-07-16 16:21:56 +0900

    先行車取得の関数を修正

--- +controller/@Dan/getFrontVehicle.m
+++ +controller/@Dan/getFrontVehicle.m
@@ -1,16 +1,20 @@
 % 分岐後の先行車を特定する関数
-function value = getFrontVehicle(obj, veh_id, road_id)
-    % route_vehsの取得
-    route_vehs = obj.RoadRouteVehsMap(road_id);
+function value = getFrontVehicle(obj, veh_id, road_id, link_id)
+    % lane_vehsの取得
+    lane_vehs = obj.RoadLinkLaneVehsMap.get(road_id, link_id);
 
     % 先行車の探索
     for value = (veh_id-1) : -1 : 1
-        if route_vehs(veh_id) == 1 || route_vehs(veh_id) == 2
-            if route_vehs(value) == 1 || route_vehs(value) == 2
+        if lane_vehs(veh_id) == 1
+            if lane_vehs(value) == 1
                 break;
             end
-        else
-            if route_vehs(value) == 3
+        elseif lane_vehs(veh_id) == 2
+            if lane_vehs(value) == 2
+                break;
+            end
+        elseif lane_vehs(veh_id) == 3
+            if lane_vehs(value) == 3
                 break;
             end
         end
