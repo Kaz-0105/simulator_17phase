779665da7476bdaae393a83959e111646ef33e2e
Kazuki Takada <kazu0105barcafan@icloud.com>
2024-08-22 15:55:49 +0900

    レーダーマップの実装の修正

--- +misc/makeRadar.m
+++ +misc/makeRadar.m
@@ -5,200 +5,174 @@ clear;
 % csvからデータを読み込む
 data_table = readtable('results/1-1_network/database.csv');
 
-% PhaseNumPerformanceMapの初期化
-PhaseNumPerformanceMap = containers.Map('KeyType', 'char', 'ValueType', 'any');
+% NumPhasesTableMapの初期化
+NumPhasesTableMap = containers.Map('KeyType', 'char', 'ValueType', 'any');
+
+% 4, 8, 17フェーズのデータを取り出す
+for num_phases = [4, 8, 17]
+    NumPhasesTableMap(num2str(num_phases)) = data_table(data_table.num_phases == num_phases, :);
+end
+
+% NumPhasesPerformanceMapの初期化
+NumPhasesPerformanceMap = containers.Map('KeyType', 'char', 'ValueType', 'any');
 
 % 固定式について
-tmp_struct = [];
+% レコードを作成
+queues = [50.1, 55.7, 65.0, 46.0, 76.7, 54.6, 60.7, 51.6, 107.4];
+delays = [33.5, 33.0, 38.1, 33.2, 47.2, 34.6, 34.4, 34.9, 49.0];
+types = {'same', 'left: high', 'straight: high', 'right: high', 'left: low', 'straight: low', 'right: low', 'main-sub', 'irregular'};
 
-tmp_queue_array = zeros(1, 9);
-tmp_queue_array(1) = 50.1;
-tmp_queue_array(2) = 55.7;
-tmp_queue_array(3) = 65.0;
-tmp_queue_array(4) = 46.0;
-tmp_queue_array(5) = 76.7;
-tmp_queue_array(6) = 54.6;
-tmp_queue_array(7) = 60.7;
-tmp_queue_array(8) = 51.6;
-tmp_queue_array(9) = 107.4;
+NumPhasesPerformanceMap('fix') = table(types', queues', delays', 'VariableNames', {'type', 'queue', 'delay'});
 
-tmp_struct.queue = tmp_queue_array;
 
-tmp_delay_array = zeros(1, 9);
-tmp_delay_array(1) = 33.5;
-tmp_delay_array(2) = 33.0;
-tmp_delay_array(3) = 38.1;
-tmp_delay_array(4) = 33.2;
-tmp_delay_array(5) = 47.2;
-tmp_delay_array(6) = 34.6;
-tmp_delay_array(7) = 34.4;
-tmp_delay_array(8) = 34.9;
-tmp_delay_array(9) = 49.0;
+% 4、8、17フェーズについて
+for num_phases = [4, 8, 17]
+    % テーブルを作成
+    tmp_performance_table = table();
 
-tmp_struct.delay = tmp_delay_array;
+    % データのテーブルを取得
+    tmp_data_table = NumPhasesTableMap(num2str(num_phases));
 
-PhaseNumPerformanceMap('fix') = tmp_struct;
+    % レコードの数を取得
+    num_records = height(tmp_data_table);
 
-% 4フェーズについて
-tmp_struct = [];
+    % データを繰り返し処理で追加
+    for record_id = 1:num_records
+        % レコードを取得
+        record = tmp_data_table(record_id, :);
 
-tmp_queue_array = zeros(1, 9);
-tmp_queue_array(1) = 28.9;
-tmp_queue_array(2) = 27.4;
-tmp_queue_array(3) = 42.0;
-tmp_queue_array(4) = 24.8;
-tmp_queue_array(5) = 26.6;
-tmp_queue_array(6) = 20.5;
-tmp_queue_array(7) = 31.0;
-tmp_queue_array(8) = 28.8;
-tmp_queue_array(9) = 65.2;
+        % inputsを定義
+        inputs = [record.input1, record.input2, record.input3, record.input4];
 
-tmp_struct.queue = tmp_queue_array;
+        % input_flagを定義
+        input_flag = isequal(inputs, inputs(1)*ones(1, 4));
 
-tmp_delay_array = zeros(1, 9);
-tmp_delay_array(1) = 20.8;
-tmp_delay_array(2) = 18.9;
-tmp_delay_array(3) = 24.5;
-tmp_delay_array(4) = 17.9;
-tmp_delay_array(5) = 19.3;
-tmp_delay_array(6) = 16.4;
-tmp_delay_array(7) = 20.8;
-tmp_delay_array(8) = 18.3;
-tmp_delay_array(9) = 28.0;
+        % relflowsを定義
+        relflows = [record.relflow1, record.relflow2, record.relflow3, record.relflow4];
 
-tmp_struct.delay = tmp_delay_array;
+        % relflow_flagを定義
+        relflow_flag = isequal(relflows, relflows(1)*ones(1, 4));
 
-PhaseNumPerformanceMap('4') = tmp_struct;
+        if input_flag
+            % レコードを作成
+            if isequal(relflows, ones(1, 4))
+                new_record = {'same', record.queue, record.delay};
+            elseif isequal(relflows, 2*ones(1, 4))
+                new_record = {'left: high', record.queue, record.delay};
+            elseif isequal(relflows, 3*ones(1, 4))
+                new_record = {'straight: high', record.queue, record.delay};
+            elseif isequal(relflows, 4*ones(1, 4))
+                new_record = {'right: high', record.queue, record.delay};
+            elseif isequal(relflows, 5*ones(1, 4))
+                new_record = {'left: low', record.queue, record.delay};
+            elseif isequal(relflows, 6*ones(1, 4))
+                new_record = {'straight: low', record.queue, record.delay};
+            elseif isequal(relflows, 7*ones(1, 4))
+                new_record = {'right: low', record.queue, record.delay};
+            else
+                continue;
+            end
 
-% 8フェーズについて
-tmp_struct = [];
+            % レコードを追加
+            tmp_performance_table(end + 1, :) = new_record;
+        elseif relflow_flag
+            % レコードを作成
+            if isequal(inputs, [900, 500, 900, 500])
+                new_record = {'main-sub', record.queue, record.delay};
+            elseif isequal(inputs, [900, 900, 500, 500])
+                new_record = {'irregular', record.queue, record.delay};
+            else
+                continue;
+            end
 
-tmp_queue_array = zeros(1, 9);
-tmp_queue_array(1) = 25.4;
-tmp_queue_array(2) = 28.1;
-tmp_queue_array(3) = 50.9;
-tmp_queue_array(4) = 19.9;
-tmp_queue_array(5) = 35.6;
-tmp_queue_array(6) = 14.2;
-tmp_queue_array(7) = 36.3;
-tmp_queue_array(8) = 20.2;
-tmp_queue_array(9) = 34.9;
+            % レコードを追加
+            tmp_performance_table(end + 1, :) = new_record;
+        end
+    end
+    % レコードのカラム名を設定
+    tmp_performance_table.Properties.VariableNames = {'type', 'queue', 'delay'};
 
-tmp_struct.queue = tmp_queue_array;
-
-tmp_delay_array = zeros(1, 9);
-tmp_delay_array(1) = 16.4;
-tmp_delay_array(2) = 18.3;
-tmp_delay_array(3) = 30.1;
-tmp_delay_array(4) = 14.9;
-tmp_delay_array(5) = 23.4;
-tmp_delay_array(6) = 11.3;
-tmp_delay_array(7) = 21.5;
-tmp_delay_array(8) = 14.8;
-tmp_delay_array(9) = 24.0;
-
-tmp_struct.delay = tmp_delay_array;
-
-PhaseNumPerformanceMap('8') = tmp_struct;
-
-% 17フェーズについて
-tmp_struct = [];
-
-tmp_queue_array = zeros(1, 9);
-tmp_queue_array(1) = 22.3;
-tmp_queue_array(2) = 22.2;
-tmp_queue_array(3) = 51.0;
-tmp_queue_array(4) = 26.0;
-tmp_queue_array(5) = 26.0;
-tmp_queue_array(6) = 14.8;
-tmp_queue_array(7) = 28.0;
-tmp_queue_array(8) = 31.3;
-tmp_queue_array(9) = 23.4;
-
-tmp_struct.queue = tmp_queue_array;
-
-tmp_delay_array = zeros(1, 9);
-tmp_delay_array(1) = 15.9;
-tmp_delay_array(2) = 15.4;
-tmp_delay_array(3) = 29.0;
-tmp_delay_array(4) = 18.6;
-tmp_delay_array(5) = 18.6;
-tmp_delay_array(6) = 11.1;
-tmp_delay_array(7) = 18.5;
-tmp_delay_array(8) = 20.1;
-tmp_delay_array(9) = 18.5;
-
-tmp_struct.delay = tmp_delay_array;
-
-PhaseNumPerformanceMap('17') = tmp_struct;
+    NumPhasesPerformanceMap(num2str(num_phases)) = tmp_performance_table;
+end
 
 % QueueLengthのレーダーチャートを作成
 f = figure(1);
 
 % 角度とラベルを生成
-theta = linspace(0, 2*pi, 10);
-label = {'same', 'left: high', 'straight: high', 'right: high', 'left: low', 'straight: low', 'right: low', 'main-sub', 'strange'};
-legend_label = {'fix', '4-phase', '8-phase', '17-phase'};
+labels = (NumPhasesPerformanceMap('fix').type)';
+theta = linspace(0, 2*pi, length(labels) + 1);
+legend_labels = {};
 
-% 固定式について
-tmp_queue_array = PhaseNumPerformanceMap('fix').queue;
-tmp_queue_array(end + 1) = tmp_queue_array(1);
-polarplot(theta, tmp_queue_array, 'LineWidth', 2);
+% 制御方式ごとに処理
+for control_method = ["fix", "4", "8", "17"]
+    control_method = char(control_method);
+
+    tmp_performance_table = NumPhasesPerformanceMap(control_method);
+
+    queue_array = zeros(1, length(labels));
+
+    for label = labels
+        label = label{1};
+
+        record = tmp_performance_table(strcmp(tmp_performance_table.type, label), :);
+
+        if ~isempty(record)
+            queue_array(find(strcmp(labels, label))) = record.queue;
+        end
+    end
+
+    queue_array(end + 1) = queue_array(1);
+    legend_labels{end + 1} = control_method;
+
+    polarplot(theta, queue_array, 'LineWidth', 2);
+    hold on;
+end
 
 ax = gca;
-ax.ThetaTickLabel = label;
-ax.ThetaTick = linspace(0, 360, 10);
+ax.ThetaTickLabel = labels;
+ax.ThetaTick = linspace(0, 360, length(labels) + 1);
 ax.FontSize = 15;
 title('Queue Length [m]');
+legend(legend_labels, 'FontSize', 15);
 
-hold on;
+hold off;
 
-% 4, 8, 17フェーズについて
-tmp_queue_array = PhaseNumPerformanceMap('4').queue;
-tmp_queue_array(end + 1) = tmp_queue_array(1);
-polarplot(theta, tmp_queue_array, 'LineWidth', 2);
-
-tmp_queue_array = PhaseNumPerformanceMap('8').queue;
-tmp_queue_array(end + 1) = tmp_queue_array(1);
-polarplot(theta, tmp_queue_array, 'LineWidth', 2);
-
-tmp_queue_array = PhaseNumPerformanceMap('17').queue;
-tmp_queue_array(end + 1) = tmp_queue_array(1);
-polarplot(theta, tmp_queue_array, 'LineWidth', 2);
-
-legend(legend_label);
-
-
-% QueueLengthのレーダーチャートを作成
+% Delay Timeのレーダーチャートを作成
 f = figure(2);
 
-% 固定式について
-tmp_delay_array = PhaseNumPerformanceMap('fix').delay;
-tmp_delay_array(end + 1) = tmp_delay_array(1);
-polarplot(theta, tmp_delay_array, 'LineWidth', 2);
+% 制御方式ごとに処理
+for control_method = ["fix", "4", "8", "17"]
+    control_method = char(control_method);
+
+    tmp_performance_table = NumPhasesPerformanceMap(control_method);
+
+    delay_array = zeros(1, length(labels));
+
+    for label = labels
+        label = label{1};
+
+        record = tmp_performance_table(strcmp(tmp_performance_table.type, label), :);
+
+        if ~isempty(record)
+            delay_array(find(strcmp(labels, label))) = record.delay;
+        end
+    end
+
+    delay_array(end + 1) = delay_array(1);
+
+    polarplot(theta, delay_array, 'LineWidth', 2);
+    hold on;
+end
 
 ax = gca;
-ax.ThetaTickLabel = label;
-ax.ThetaTick = linspace(0, 360, 10);
+ax.ThetaTickLabel = labels;
+ax.ThetaTick = linspace(0, 360, length(labels) + 1);
 ax.FontSize = 15;
 title('Delay Time [s]');
+legend(legend_labels, 'FontSize', 15);
 
-hold on;
+hold off;
 
-% 4, 8, 17フェーズについて
-tmp_delay_array = PhaseNumPerformanceMap('4').delay;
-tmp_delay_array(end + 1) = tmp_delay_array(1);
-polarplot(theta, tmp_delay_array, 'LineWidth', 2);
-
-tmp_delay_array = PhaseNumPerformanceMap('8').delay;
-tmp_delay_array(end + 1) = tmp_delay_array(1);
-polarplot(theta, tmp_delay_array, 'LineWidth', 2);
-
-tmp_delay_array = PhaseNumPerformanceMap('17').delay;
-tmp_delay_array(end + 1) = tmp_delay_array(1);
-polarplot(theta, tmp_delay_array, 'LineWidth', 2);
-
-legend(legend_label);
 
 
 
