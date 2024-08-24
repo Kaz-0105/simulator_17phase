2667c2b8f92b3ce5f581df8b9ad0623b3b581566
Kazuki Takada <kazu0105barcafan@icloud.com>
2024-08-24 21:22:41 +0900

    実験

--- +misc/makeRadar.m
+++ +misc/makeRadar.m
@@ -3,7 +3,7 @@ close all;
 clear;
 
 % csvからデータを読み込む
-data_table = readtable('results/1-1_network/database.csv');
+data_table = readtable('results/1-1_network/database500.csv');
 
 % NumPhasesTableMapの初期化
 NumPhasesTableMap = containers.Map('KeyType', 'char', 'ValueType', 'any');
@@ -44,134 +44,182 @@ for num_phases = [4, 8, 17]
         % inputsを定義
         inputs = [record.input1, record.input2, record.input3, record.input4];
 
-        % input_flagを定義
-        input_flag = isequal(inputs, inputs(1)*ones(1, 4));
-
         % relflowsを定義
         relflows = [record.relflow1, record.relflow2, record.relflow3, record.relflow4];
 
-        % relflow_flagを定義
-        relflow_flag = isequal(relflows, relflows(1)*ones(1, 4));
-
-        if input_flag
+        if isequal(inputs, 700*ones(1, 4))
             % レコードを作成
             if isequal(relflows, ones(1, 4))
-                new_record = {'same', record.queue, record.delay};
+                new_record = {'same', 'same', record.queue, record.delay};
             elseif isequal(relflows, 2*ones(1, 4))
-                new_record = {'left: high', record.queue, record.delay};
+                new_record = {'same', 'left: high', record.queue, record.delay};
             elseif isequal(relflows, 3*ones(1, 4))
-                new_record = {'straight: high', record.queue, record.delay};
+                new_record = {'same', 'straight: high', record.queue, record.delay};
             elseif isequal(relflows, 4*ones(1, 4))
-                new_record = {'right: high', record.queue, record.delay};
+                new_record = {'same', 'right: high', record.queue, record.delay};
             elseif isequal(relflows, 5*ones(1, 4))
-                new_record = {'left: low', record.queue, record.delay};
+                new_record = {'same', 'left: low', record.queue, record.delay};
             elseif isequal(relflows, 6*ones(1, 4))
-                new_record = {'straight: low', record.queue, record.delay};
+                new_record = {'same', 'straight: low', record.queue, record.delay};
             elseif isequal(relflows, 7*ones(1, 4))
-                new_record = {'right: low', record.queue, record.delay};
+                new_record = {'same', 'right: low', record.queue, record.delay};
+            elseif isequal(relflows, 8*ones(1, 4))
+                new_record = {'same', 'left only', record.queue, record.delay};
             else
                 continue;
             end
 
-            % レコードを追加
-            tmp_performance_table(end + 1, :) = new_record;
-        elseif relflow_flag
+        elseif isequal(inputs, [900, 500, 900, 500])
             % レコードを作成
-            if isequal(inputs, [900, 500, 900, 500])
-                new_record = {'main-sub', record.queue, record.delay};
-            elseif isequal(inputs, [900, 900, 500, 500])
-                new_record = {'irregular', record.queue, record.delay};
+            if isequal(relflows, ones(1, 4))
+                new_record = {'main-sub', 'same', record.queue, record.delay};
+            elseif isequal(relflows, 2*ones(1, 4))
+                new_record = {'main-sub', 'left: high', record.queue, record.delay};
+            elseif isequal(relflows, 3*ones(1, 4))
+                new_record = {'main-sub', 'straight: high', record.queue, record.delay};
+            elseif isequal(relflows, 4*ones(1, 4))
+                new_record = {'main-sub', 'right: high', record.queue, record.delay};
+            elseif isequal(relflows, 5*ones(1, 4))
+                new_record = {'main-sub', 'left: low', record.queue, record.delay};
+            elseif isequal(relflows, 6*ones(1, 4))
+                new_record = {'main-sub', 'straight: low', record.queue, record.delay};
+            elseif isequal(relflows, 7*ones(1, 4))
+                new_record = {'main-sub', 'right: low', record.queue, record.delay};
+            elseif isequal(relflows, 8*ones(1, 4))
+                new_record = {'main-sub', 'left only', record.queue, record.delay};
             else
                 continue;
             end
-
-            % レコードを追加
-            tmp_performance_table(end + 1, :) = new_record;
+        elseif isequal(inputs, [900, 900, 500, 500])
+            % レコードを作成
+            if isequal(relflows, ones(1, 4))
+                new_record = {'irregular', 'same', record.queue, record.delay};
+            elseif isequal(relflows, 2*ones(1, 4))
+                new_record = {'irregular', 'left: high', record.queue, record.delay};
+            elseif isequal(relflows, 3*ones(1, 4))
+                new_record = {'irregular', 'straight: high', record.queue, record.delay};
+            elseif isequal(relflows, 4*ones(1, 4))
+                new_record = {'irregular', 'right: high', record.queue, record.delay};
+            elseif isequal(relflows, 5*ones(1, 4))
+                new_record = {'irregular', 'left: low', record.queue, record.delay};
+            elseif isequal(relflows, 6*ones(1, 4))
+                new_record = {'irregular', 'straight: low', record.queue, record.delay};
+            elseif isequal(relflows, 7*ones(1, 4))
+                new_record = {'irregular', 'right: low', record.queue, record.delay};
+            elseif isequal(relflows, 8*ones(1, 4))
+                new_record = {'irregular', 'left only', record.queue, record.delay};
+            else
+                continue;
+            end
+        else
+            continue;
         end
+
+        % レコードを追加
+        tmp_performance_table(end + 1, :) = new_record;
     end
     % レコードのカラム名を設定
-    tmp_performance_table.Properties.VariableNames = {'type', 'queue', 'delay'};
+    tmp_performance_table.Properties.VariableNames = {'inflow_type', 'route_type', 'queue', 'delay'};
 
     NumPhasesPerformanceMap(num2str(num_phases)) = tmp_performance_table;
 end
+% 流入量と旋回率のタイプを取得
+route_types = unique(NumPhasesPerformanceMap('4').route_type)';
+inflow_types = unique(NumPhasesPerformanceMap('4').inflow_type)';
 
-% QueueLengthのレーダーチャートを作成
-f = figure(1);
+for inflow_type = inflow_types
+    % セル配列から取り出し
+    inflow_type = inflow_type{1};
 
-% 角度とラベルを生成
-labels = (NumPhasesPerformanceMap('fix').type)';
-theta = linspace(0, 2*pi, length(labels) + 1);
-legend_labels = {};
+    % QueueLengthのレーダーチャートを作成
+    figure();
 
-% 制御方式ごとに処理
-for control_method = ["4", "8", "17"]
-    control_method = char(control_method);
+    % 角度とラベルを生成
+    theta = linspace(0, 2*pi, length(route_types) + 1);
+    legend_labels = {};
 
-    tmp_performance_table = NumPhasesPerformanceMap(control_method);
+    % 制御方式ごとに処理
+    for control_method = ["4", "8", "17"]
+        control_method = char(control_method);
 
-    queue_array = zeros(1, length(labels));
+        tmp_performance_table = NumPhasesPerformanceMap(control_method);
+        condition = strcmp(tmp_performance_table.inflow_type, inflow_type);
+        tmp_performance_table = tmp_performance_table(condition, :);
 
-    for label = labels
-        label = label{1};
+        queue_array = zeros(1, length(route_types));
 
-        record = tmp_performance_table(strcmp(tmp_performance_table.type, label), :);
+        for route_type = route_types
+            % セル配列から取り出し
+            route_type = route_type{1};
 
-        if ~isempty(record)
-            queue_array(find(strcmp(labels, label))) = record.queue;
+            condition = strcmp(tmp_performance_table.route_type, route_type);
+
+            record = tmp_performance_table(condition, :);
+
+            if ~isempty(record)
+                condition = strcmp(route_types, route_type);
+                queue_array(find(condition)) = record.queue;
+            end
         end
+
+        queue_array(end + 1) = queue_array(1);
+        legend_labels{end + 1} = control_method;
+
+        polarplot(theta, queue_array, 'LineWidth', 2);
+        hold on;
     end
 
-    queue_array(end + 1) = queue_array(1);
-    legend_labels{end + 1} = control_method;
+    ax = gca;
+    ax.ThetaTickLabel = route_types;
+    ax.ThetaTick = linspace(0, 360, length(route_types) + 1);
+    ax.FontSize = 20;
+    title('Queue Length [m]');
+    legend(legend_labels, 'FontSize', 15);
 
-    polarplot(theta, queue_array, 'LineWidth', 2);
-    hold on;
-end
+    hold off;
 
-ax = gca;
-ax.ThetaTickLabel = labels;
-ax.ThetaTick = linspace(0, 360, length(labels) + 1);
-ax.FontSize = 15;
-title('Queue Length [m]');
-legend(legend_labels, 'FontSize', 15);
+    % Delay Timeのレーダーチャートを作成
+    figure();
 
-hold off;
+    % 制御方式ごとに処理
+    for control_method = ["4", "8", "17"]
+        control_method = char(control_method);
 
-% Delay Timeのレーダーチャートを作成
-f = figure(2);
+        tmp_performance_table = NumPhasesPerformanceMap(control_method);
+        condition = strcmp(tmp_performance_table.inflow_type, inflow_type);
+        tmp_performance_table = tmp_performance_table(condition, :);
 
-% 制御方式ごとに処理
-for control_method = ["4", "8", "17"]
-    control_method = char(control_method);
+        delay_array = zeros(1, length(route_types));
 
-    tmp_performance_table = NumPhasesPerformanceMap(control_method);
+        for route_type = route_types
+            route_type = route_type{1};
 
-    delay_array = zeros(1, length(labels));
+            condition = strcmp(tmp_performance_table.route_type, route_type);
+            
+            record = tmp_performance_table(condition, :);
 
-    for label = labels
-        label = label{1};
-
-        record = tmp_performance_table(strcmp(tmp_performance_table.type, label), :);
-
-        if ~isempty(record)
-            delay_array(find(strcmp(labels, label))) = record.delay;
+            if ~isempty(record)
+                condition = strcmp(route_types, route_type);
+                delay_array(find(condition)) = record.delay;
+            end
         end
+
+        delay_array(end + 1) = delay_array(1);
+
+        polarplot(theta, delay_array, 'LineWidth', 2);
+        hold on;
     end
 
-    delay_array(end + 1) = delay_array(1);
+    ax = gca;
+    ax.ThetaTickLabel = route_types;
+    ax.ThetaTick = linspace(0, 360, length(route_types) + 1);
+    ax.FontSize = 20;
+    title('Delay Time [s]');
+    fprintf('Inflow Type: %s\n', inflow_type);
+    legend(legend_labels, 'FontSize', 15);
 
-    polarplot(theta, delay_array, 'LineWidth', 2);
-    hold on;
+    hold off;
 end
-
-ax = gca;
-ax.ThetaTickLabel = labels;
-ax.ThetaTick = linspace(0, 360, length(labels) + 1);
-ax.FontSize = 15;
-title('Delay Time [s]');
-legend(legend_labels, 'FontSize', 15);
-
-hold off;
 
 
 
