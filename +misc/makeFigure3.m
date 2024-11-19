b553876f4bd8229d483c836282edbb596f3dda19
Kazuki Takada <kazu0105barcafan@icloud.com>
2024-11-19 22:38:35 +0900

    scoot追加

--- +misc/makeFigure3.m
+++ +misc/makeFigure3.m
@@ -32,6 +32,9 @@ queue_17 = tmp_data_table.queue(tmp_data_table.num_phases == 17);
 delay_17 = tmp_data_table.delay(tmp_data_table.num_phases == 17);
 calc_time_17 = tmp_data_table.calc_time(tmp_data_table.num_phases == 17);
 
+queue_scoot = [39.6, 20.7, 32.3, 35.2, 33.6, 49.6, 34.8]';
+delay_scoot = [32.2, 22.9, 30.7, 36.9, 30.4, 40.1, 34.6]';
+
 % データのバリデーション
 if length(queue_4) ~= EXPERIMENT_CONDITION_NUM
     error('Number of 4-phase experiments is lower than expected.');
@@ -75,22 +78,22 @@ end
 % データのプロット
 figure;
 
-bar([queue_4, queue_8, queue_17]);
+bar([queue_scoot, queue_4, queue_8, queue_17]);
 xticklabels(relflow1_labels);
 xlabel('Route selection type', 'FontSize', LABEL_FONT_SIZE);
 ylabel('Queue', 'FontSize', LABEL_FONT_SIZE);
 title('Queue - Route selection type', 'FontSize', TITLE_FONT_SIZE);
-legend({'4-phase', '8-phase', '17-phase'}, 'FontSize', LEGEND_FONT_SIZE);
+legend({'scoot', '4-phase', '8-phase', '17-phase'}, 'FontSize', LEGEND_FONT_SIZE);
 set(gca, 'FontSize', AXIS_FONT_SIZE);
 
 figure;
 
-bar([delay_4, delay_8, delay_17]);
+bar([delay_scoot, delay_4, delay_8, delay_17]);
 xticklabels(relflow1_labels);
 xlabel('Route selection type', 'FontSize', LABEL_FONT_SIZE);
 ylabel('Delay Time', 'FontSize', LABEL_FONT_SIZE);
 title('Delay Time - Route selection type', 'FontSize', TITLE_FONT_SIZE);
-legend({'4-phase', '8-phase', '17-phase'}, 'FontSize', LEGEND_FONT_SIZE);
+legend({'scoot', '4-phase', '8-phase', '17-phase'}, 'FontSize', LEGEND_FONT_SIZE);
 set(gca, 'FontSize', AXIS_FONT_SIZE);
 
 figure;
