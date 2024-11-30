94d041d4584db139e7fd5803d39dd34786ab3c31
Kazuki Takada <kazu0105barcafan@icloud.com>
2024-11-30 19:27:10 +0900

    変化が起きたかの時相論理をフェーズベースではなく信号機ベースに変更

--- +controller/@Dan/makeConstraints.m
+++ +controller/@Dan/makeConstraints.m
@@ -49,9 +49,10 @@ function makeConstraints(obj)
 
     % 信号機の変数が増えた分の変数を追加
     [row_num, ~] = size(obj.milp_matrices.P);
-    obj.milp_matrices.P = [obj.milp_matrices.P, zeros(row_num, obj.tmp_phase_num*obj.N_p + (obj.tmp_phase_num + 1)*(obj.N_p-1))];
+    obj.milp_matrices.P = [obj.milp_matrices.P, zeros(row_num, obj.tmp_phase_num*obj.N_p + (obj.signal_num + 1)*(obj.N_p-1))];
     [~, obj.variables_size] = size(obj.milp_matrices.P);
     
+    % フェーズの変数の定義
     for phase_id = 1: obj.tmp_phase_num
         phase_group = obj.PhaseSignalGroupMap(phase_id);
 
@@ -72,26 +73,22 @@ function makeConstraints(obj)
     end
 
     % 信号現示の変化のバイナリ変数を定義 
-    for step = 1:(obj.N_p-1)
-        P_tmp = zeros(4*obj.tmp_phase_num, obj.variables_size);
+    for step = 1:(obj.N_p - 1)
+        P_tmp = [];
+        q_tmp = [];
+        for signal_id = 1: obj.signal_num
+            p_tmp = zeros(4, obj.variables_size);
+            p_tmp(:, signal_id + obj.v_length*(step - 1)) = [1; -1; -1; 1];
+            p_tmp(:, signal_id + obj.v_length*step) = [1; -1; 1; -1];
+            p_tmp(:, obj.v_length*obj.N_p + obj.tmp_phase_num*obj.N_p + signal_id + (obj.signal_num + 1)*(step - 1)) = [1; 1; -1; -1];
 
-        for phase_id = 1: obj.tmp_phase_num
-            P_tmp((1 + 4*(phase_id -1): 4*phase_id), obj.v_length*obj.N_p + phase_id + obj.tmp_phase_num*(step -1)) = [1;-1;-1;1];
-            P_tmp((1 + 4*(phase_id -1): 4*phase_id), obj.v_length*obj.N_p + phase_id + obj.tmp_phase_num*step) = [1;-1;1;-1];
-            P_tmp((1 + 4*(phase_id -1): 4*phase_id), obj.v_length*obj.N_p + obj.tmp_phase_num*obj.N_p + phase_id + (obj.tmp_phase_num + 1)*(step - 1)) = [1;1;-1;-1];
+            P_tmp = [P_tmp; p_tmp];
+            q_tmp = [q_tmp; 2; 0; 0; 0];
         end
 
         obj.milp_matrices.P = [obj.milp_matrices.P; P_tmp];
-
-        q_tmp = zeros(4*obj.tmp_phase_num, 1);
-
-        for phase_id = 1: obj.tmp_phase_num
-            q_tmp(1 + 4*(phase_id -1),1) = 2;    
-        end
-
         obj.milp_matrices.q = [obj.milp_matrices.q; q_tmp];
     end
-
 
     % 青になっていい信号の数の制限
     for step = 1:obj.N_p
@@ -102,15 +99,24 @@ function makeConstraints(obj)
         obj.milp_matrices.q = [obj.milp_matrices.q; obj.road_num];
     end
 
+    % 青になっていいフェーズの数の制限
+    for step = 1:obj.N_p
+        P_tmp = zeros(1, obj.variables_size);
+        for phase_id = 1: obj.tmp_phase_num
+            P_tmp(1, obj.v_length*obj.N_p + phase_id + obj.tmp_phase_num*(step-1)) = 1;
+        end
+        obj.milp_matrices.Peq = [obj.milp_matrices.Peq; P_tmp];
+        obj.milp_matrices.qeq = [obj.milp_matrices.qeq; 1];
+    end
+
     % 信号の変化の回数の制限
     P_tmp = zeros(1, obj.variables_size);
 
     for step = 1:(obj.N_p-1)
-        P_tmp(1, obj.v_length*obj.N_p + obj.tmp_phase_num*obj.N_p + (obj.tmp_phase_num + 1)*step) = 1;
+        P_tmp(1, obj.v_length*obj.N_p + obj.tmp_phase_num*obj.N_p + (obj.signal_num + 1)*step) = 1;
     end
 
     obj.milp_matrices.P = [obj.milp_matrices.P; P_tmp];
-
     obj.milp_matrices.q = [obj.milp_matrices.q; obj.m];
 
     % delta_cの固定
@@ -127,15 +133,21 @@ function makeConstraints(obj)
         end
     end
 
-
-    % 増えた変数の定義その２ 
+    % 信号機全体で変化しているかのバイナリの定義
     for step = 1:(obj.N_p-1)
-        P_tmp = zeros(1, obj.variables_size);
-        P_tmp(obj.v_length*obj.N_p + obj.tmp_phase_num*obj.N_p + (obj.tmp_phase_num + 1)*(step -1) + 1 : obj.v_length*obj.N_p + obj.tmp_phase_num*obj.N_p+ (obj.tmp_phase_num + 1)*step) = [-ones(1, obj.tmp_phase_num), 2];
-        obj.milp_matrices.Peq = [obj.milp_matrices.Peq; P_tmp];
-        obj.milp_matrices.qeq = [obj.milp_matrices.qeq; 0];
-    end
-    
+        P_tmp = zeros(2, obj.variables_size);
+
+        for signal_id = 1: obj.signal_num
+            P_tmp(:, obj.v_length*obj.N_p + obj.tmp_phase_num*obj.N_p + signal_id + (obj.signal_num + 1)*(step - 1)) = [-1; 1];
+        end
+
+        P_tmp(:, obj.v_length*obj.N_p + obj.tmp_phase_num*obj.N_p + (obj.signal_num + 1)*step) = [1; -obj.road_num*(obj.road_num - 1)];
+
+        q_tmp = [0; 0];
+
+        obj.milp_matrices.P = [obj.milp_matrices.P; P_tmp];
+        obj.milp_matrices.q = [obj.milp_matrices.q; q_tmp];
+    end 
 
     % 初期値の固定
     for step = 1:obj.num_fix_steps
@@ -156,7 +168,7 @@ function makeConstraints(obj)
         q_tmp = 1;
 
         for i = 1: step
-            P_tmp(1, obj.v_length*obj.N_p + obj.tmp_phase_num*obj.N_p + (obj.tmp_phase_num + 1)*i) = 1;
+            P_tmp(1, obj.v_length*obj.N_p + obj.tmp_phase_num*obj.N_p + (obj.signal_num + 1)*i) = 1;
         end
 
         phi_past_data = obj.PhiResults.get('past_data');
@@ -167,8 +179,5 @@ function makeConstraints(obj)
 
         obj.milp_matrices.P = [obj.milp_matrices.P; P_tmp];
         obj.milp_matrices.q = [obj.milp_matrices.q; q_tmp];
-
     end
-
-
 end
