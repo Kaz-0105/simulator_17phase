e44a45ba22fb1078d28b5e4dad79e8a93bca78af
Kazuki Takada <kazu0105barcafan@icloud.com>
2024-12-07 17:21:24 +0900

    MSCS用のグラフを作成

---
+++ +misc/makeFigureMscs.m
@@ -0,0 +1,90 @@
+clear;
+close all;
+
+NUM_EXPERIMENT_CONDITIONS = 7;
+
+relflow_labels = [...
+    "Uniform", ...
+    "Left Heavy", ...
+    "Straight Heavy", ...
+    "Right Heavy", ...
+    "Left Light", ...
+    "Straight Light", ...
+    "Right Light" ...
+];
+
+file_names = [
+    "balanced", ...
+    "unbalanced",... 
+    "main_sub" ...
+];
+
+FileTitleMap = containers.Map( ...
+    {'balanced', 'unbalanced', 'main_sub'}, ...
+    {'Balanced Traffic Condition', 'Unbalanced Traffic Condition', 'Main-Sub Traffic Condition'} ...
+);
+
+TITLE_FONT_SIZE = 20;
+LABEL_FONT_SIZE = 20;
+AXIS_FONT_SIZE = 20;
+LEGEND_FONT_SIZE = 20;
+
+
+for file_name = file_names
+    % csvから実験データを読み込む
+    results = readtable("results/1-1_network/" + file_name + ".csv");
+    fields = {
+        'num_phases', 'relflow1', 'relflow2', 'relflow3', 'relflow4', 'queue', 'delay', 'calc_time'
+    };
+    results = results(:, fields);
+    results = sortrows( ...
+        results, ...
+        {'relflow1', 'relflow2', 'relflow3', 'relflow4', 'num_phases'}, ... 
+        {'ascend', 'ascend', 'ascend', 'ascend', 'ascend'} ...
+    );
+
+    PhaseDataMap = containers.Map('KeyType', 'int32', 'ValueType', 'any');
+    for num_phases = unique(results.num_phases)'
+        PhaseDataMap(num_phases) = struct(...
+            'relflow_id', results.relflow1(results.num_phases == num_phases), ...
+            'queue', results.queue(results.num_phases == num_phases), ...
+            'delay', results.delay(results.num_phases == num_phases), ...
+            'calc_time', results.calc_time(results.num_phases == num_phases) ...
+        );
+
+        if sum(results.num_phases == num_phases) ~= NUM_EXPERIMENT_CONDITIONS
+            error('Number of %d-phase experiments in the %s situation is lower or higher than expected.', num_phases, file_name);
+        end
+    end
+
+    % 信号待ちの車列の結果のプロット
+    figure;
+    bar([PhaseDataMap(4).queue, PhaseDataMap(8).queue, PhaseDataMap(17).queue]);
+    xticklabels(relflow_labels(PhaseDataMap(4).relflow_id));
+    xlabel('Route Selection Type', 'FontSize', LABEL_FONT_SIZE);
+    ylabel('Average Queue Length [m]', 'FontSize', LABEL_FONT_SIZE);
+    title(FileTitleMap(file_name), 'FontSize', TITLE_FONT_SIZE);
+    legend({'4-phase MPC', '8-phase MPC', '17-phase MPC'}, 'FontSize', LEGEND_FONT_SIZE);
+    set(gca, 'FontSize', AXIS_FONT_SIZE);
+
+    % 遅れ時間の結果のプロット
+    figure;
+    bar([PhaseDataMap(4).delay, PhaseDataMap(8).delay, PhaseDataMap(17).delay]);
+    xticklabels(relflow_labels(PhaseDataMap(4).relflow_id));
+    xlabel('Route Selection Type', 'FontSize', LABEL_FONT_SIZE);
+    ylabel('Average Delay Time [s]', 'FontSize', LABEL_FONT_SIZE);
+    title(FileTitleMap(file_name), 'FontSize', TITLE_FONT_SIZE);
+    legend({'4-phase MPC', '8-phase MPC', '17-phase MPC'}, 'FontSize', LEGEND_FONT_SIZE);
+    set(gca, 'FontSize', AXIS_FONT_SIZE);
+
+    % 計算時間の結果のプロット
+    figure;
+    bar([PhaseDataMap(4).calc_time, PhaseDataMap(8).calc_time, PhaseDataMap(17).calc_time]);
+    xticklabels(relflow_labels(PhaseDataMap(4).relflow_id));
+    xlabel('Route Selection Type', 'FontSize', LABEL_FONT_SIZE);
+    ylabel('Average Calculation Time [s]', 'FontSize', LABEL_FONT_SIZE);
+    title(FileTitleMap(file_name), 'FontSize', TITLE_FONT_SIZE);
+    legend({'4-phase MPC', '8-phase MPC', '17-phase MPC'}, 'FontSize', LEGEND_FONT_SIZE);
+    set(gca, 'FontSize', AXIS_FONT_SIZE);
+
+end
